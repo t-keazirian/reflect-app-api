b999c621b366521001a353037b84e7575abd965a
Taylor Keazirian <taylork26@gmail.com>
2021-04-28 22:46:33 -0500

    fixed slash

--- src/config.js
+++ src/config.js
@@ -11,6 +11,6 @@ module.exports = {
 	TEST_DATABASE_URL:
 		process.env.TEST_DATABASE_URL ||
 		'postgresql://dunder_mifflin@localhost/reflections-test',
-	CLIENT_ORIGIN: (process.env.NODE_ENV === 'development') ? '*':'https://reflect-app-rouge.vercel.app/',
+	CLIENT_ORIGIN: (process.env.NODE_ENV === 'development') ? '*':'https://reflect-app-rouge.vercel.app',
 	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000',
 };
